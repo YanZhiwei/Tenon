479efc77c21431d17886d477fb5f3ca3d07e5c8f
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-01-10 23:07:44 +0800

    add sample

---
+++ src/Tenon.CentralizedConfiguration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tenon.CentralizedConfiguration
+{
+    /// <summary>
+    /// https://learn.microsoft.com/zh-cn/dotnet/architecture/cloud-native/centralized-configuration
+    /// </summary>
+    public class Class1
+    {
+
+    }
+}
