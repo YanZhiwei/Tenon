65b391a80c9ffbc993e1093d32b35bfcd8882e87
Yan Zhiwei <yan.zhiwei@hotmail.com>
2024-01-12 18:29:46 +0800

    fix

--- src/Tenon.DistributedLocker.Redis/RedisLockerTimer.cs
+++ src/Tenon.DistributedLocker.Redis/RedisLockerTimer.cs
@@ -4,11 +4,13 @@ using Timer = System.Timers.Timer;
 
 namespace Tenon.DistributedLocker.Redis;
 
-internal sealed class RedisLockerTimer : Timer
+internal sealed class RedisLockerTimer
 {
     private static readonly string Prefix;
 
     public readonly string UniqueLockId;
+
+    private readonly Timer _innerTimer;
 
     static RedisLockerTimer()
     {
@@ -16,9 +18,12 @@ internal sealed class RedisLockerTimer : Timer
         Prefix = $"{Environment.MachineName}_{currentProcess.Id}";
     }
 
-    public RedisLockerTimer(string cacheKey)
+    public RedisLockerTimer(string cacheKey, TimerCallback timerCallback, int timeoutSeconds = 5)
     {
         Checker.Begin().NotNullOrEmpty(cacheKey, nameof(cacheKey));
         UniqueLockId = $"{Prefix}_{cacheKey}";
+        var expiration = TimeSpan.FromSeconds(timeoutSeconds);
+        var refreshMilliseconds = (int)(expiration.TotalMilliseconds / 2.0);
+        _innerTimer = new System.Timers.Timer(() => { }, null, refreshMilliseconds, refreshMilliseconds);
     }
 }
