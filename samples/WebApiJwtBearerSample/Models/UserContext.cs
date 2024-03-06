0b13fd88185c317a669f690a2e1426feb15c6620
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-03-06 23:37:52 +0800

    完善Jwt认证示例 (#23)

---
+++ samples/WebApiJwtBearerSample/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiJwtBearerSample.Models
+{
+    public sealed class UserContext
+    {
+        public long Id { get; set; }
+        public string Account { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string RoleIds { get; set; } = string.Empty;
+        public string RemoteIpAddress { get; set; } = string.Empty;
+    }
+}
---
+++ src/Tenon.AspNetCore/Authorization/AuthorizeScopeAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Tenon.AspNetCore.Authorization;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeScopeAttribute(string[] codes) : AuthorizeAttribute
+{
+    public string[] Codes { get; set; } = codes;
+}
