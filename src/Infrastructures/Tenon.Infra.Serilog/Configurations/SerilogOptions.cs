0e0712f528cbddd71e3e6bd7484a0b93feaa598a
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-03-08 00:43:36 +0800

    增加Serilog初步扩展

---
+++ src/Infrastructures/Tenon.Infra.Serilog/Configurations/SerilogOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Tenon.Infra.Serilog.Configurations;
+
+public sealed class SerilogOptions
+{
+    public bool IsClearProviders { get; set; }
+
+    public string Application { get; set; }
+}
---
+++ src/Infrastructures/Tenon.Infra.Serilog/Enrichers/AspNetCoreEnvironmentEnricher.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Tenon.Infra.Serilog.Enrichers;
+
+internal class AspNetCoreEnvironmentEnricher : ILogEventEnricher
+{
+    private static readonly string AspNetEnvironment;
+
+    static AspNetCoreEnvironmentEnricher()
+    {
+        AspNetEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    }
+
+    public void Enrich(
+        LogEvent logEvent,
+        ILogEventPropertyFactory propertyFactory)
+    {
+        var enrichProperty = propertyFactory
+            .CreateProperty(
+                "AspNetCoreEnvironment",
+                AspNetEnvironment);
+
+        logEvent.AddOrUpdateProperty(enrichProperty);
+    }
+}
