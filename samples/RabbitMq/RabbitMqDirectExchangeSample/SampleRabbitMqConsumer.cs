2a6b27950c5ac72ec60de0017f5f6330c0dcfc19
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-03-19 23:56:19 +0800

    完善RabbitMq以及增加对应Sample (#30)

---
+++ samples/RabbitMq/RabbitMqDirectExchangeSample/SampleRabbitMqConsumer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using Tenon.Infra.RabbitMq;
+using Tenon.Infra.RabbitMq.Models;
+
+namespace RabbitMqDirectExchangeSample;
+
+public class SampleRabbitMqConsumer(RabbitMqConnection rabbitMqConnection, ILogger<RabbitMqConsumer> logger)
+    : RabbitMqConsumer(rabbitMqConnection, logger)
+{
+    protected override RabbitMqQueue GetRabbitMqQueue()
+    {
+        return new RabbitMqQueue("log_all", "MyExchange", "error", autoAck: false);
+    }
+
+    public override Task<bool> BasicConsumerReceived(string exchange, string routingKey, byte[] body,
+        RabbitMqQueue queue)
+    {
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"接收成功！【{message}】，发送短信通知");
+        return Task.FromResult(true);
+    }
+}
--- src/Infrastructures/Tenon.Infra.RabbitMq/RabbitMqConsumer.cs
+++ src/Infrastructures/Tenon.Infra.RabbitMq/RabbitMqConsumer.cs
@@ -2,7 +2,6 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Tenon.Infra.RabbitMq.Configurations;
-using Tenon.Infra.RabbitMq.Extensions;
 using Tenon.Infra.RabbitMq.Models;
 
 namespace Tenon.Infra.RabbitMq;
@@ -10,83 +9,81 @@ namespace Tenon.Infra.RabbitMq;
 public abstract class RabbitMqConsumer : IDisposable
 {
     private static readonly object Lock = new();
-    private readonly IModel _channel;
     protected readonly ILogger<RabbitMqConsumer> Logger;
     protected readonly RabbitMqConnection RabbitMqConnection;
     protected readonly RabbitMqOptions RabbitMqOptions;
+    private IModel _channel;
 
-    private RabbitMqBroker? _rabbitMqBroker;
+    private RabbitMqQueue? _rabbitMqQueue;
 
     protected RabbitMqConsumer(RabbitMqConnection rabbitMqConnection,
         ILogger<RabbitMqConsumer> logger)
     {
         RabbitMqConnection = rabbitMqConnection ?? throw new ArgumentNullException(nameof(rabbitMqConnection));
         Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _channel = RabbitMqConnection.Connection.CreateModel();
         RabbitMqOptions = rabbitMqConnection.Options;
     }
 
-    protected RabbitMqBroker RabbitMqBroker
+    protected RabbitMqQueue RabbitMqQueue
     {
         get
         {
-            if (_rabbitMqBroker == null)
+            if (_rabbitMqQueue == null)
                 lock (Lock)
                 {
-                    if (_rabbitMqBroker == null) _rabbitMqBroker = GetRabbitMqBroker();
+                    if (_rabbitMqQueue == null)
+                        _rabbitMqQueue = GetRabbitMqQueue();
                 }
 
-            return _rabbitMqBroker;
+            return _rabbitMqQueue;
         }
     }
 
     public void Dispose()
     {
-        RabbitMqConnection.Dispose();
+        _channel?.Dispose();
     }
 
-    protected abstract RabbitMqBroker GetRabbitMqBroker();
+    protected abstract RabbitMqQueue GetRabbitMqQueue();
 
-    protected abstract Task<bool> BasicConsumerReceived(string exchange, string routingKey, byte[] body,
+    public abstract Task<bool> BasicConsumerReceived(string exchange, string routingKey, byte[] body,
         RabbitMqQueue queue);
 
-    public virtual void BasicConsumer(params string[] routeKeys)
+    public virtual void BasicConsumer()
     {
-        if ((routeKeys?.Any() ?? false) == false)
-            throw new ArgumentNullException(nameof(routeKeys));
+        if (RabbitMqQueue == null)
+            throw new ArgumentNullException(nameof(RabbitMqQueue));
+        if (!RabbitMqConnection.IsConnected)
+            RabbitMqConnection.TryConnect();
+        _channel = RabbitMqConnection.Connection.CreateModel();
+        _channel.QueueDeclare(RabbitMqQueue.Name,
+            RabbitMqQueue.Durable,
+            RabbitMqQueue.Exclusive,
+            RabbitMqQueue.AutoDelete,
+            null);
+        Logger.LogDebug(
+            $"RabbitMq:[{RabbitMqQueue.Name}] queueName: {RabbitMqQueue.Name} declare succeeded.");
 
+        var consumer = new EventingBasicConsumer(_channel);
 
-        var queues = RabbitMqBroker.Declare(_channel, routeKeys)?.ToArray();
-
-        if (queues?.Any() ?? false)
+        consumer.Received += async (model, ea) =>
         {
-            foreach (var queue in queues)
-                Logger.LogDebug(
-                    $"RabbitMq:[{RabbitMqOptions.Name}] queueName {queue.Name},routeKey:{queue.RoutingKey} bind succeeded.");
+            var currentQueue = ((EventingBasicConsumer)model!).Model.CurrentQueue;
+            Logger.LogDebug(
+                $"RabbitMq:[{RabbitMqOptions.Name}] message received,queue:{currentQueue},routeKey:{ea.RoutingKey}");
 
-            var consumer = new EventingBasicConsumer(_channel);
-            foreach (var queue in queues)
-            {
-                _channel.BasicConsume(queue.Name, queue.AutoAck, queue.ConsumerTag, false, queue.Exclusive,
-                    queue.Arguments, consumer);
-                Logger.LogDebug(
-                    $"RabbitMq:[{RabbitMqOptions.Name}] queueName:{queue.Name} consumer succeeded,routeKey:{queue.RoutingKey}");
-            }
+            var result = await BasicConsumerReceived(ea.Exchange, ea.RoutingKey, ea.Body.ToArray(), RabbitMqQueue);
+            if (RabbitMqQueue.AutoAck) return;
+            if (result)
+                _channel.BasicAck(ea.DeliveryTag, RabbitMqQueue.AckMultiple);
+            else
+                _channel.BasicReject(ea.DeliveryTag, RabbitMqQueue.RejectRequeue);
+        };
 
-            var dic = queues.ToDictionary(item => item.RoutingKey, item => item);
-
-            consumer.Received += async (model, ea) =>
-            {
-                Logger.LogDebug($"RabbitMq:[{RabbitMqOptions.Name}] message received,routeKey:{ea.RoutingKey}");
-                var queue = dic[ea.RoutingKey];
-                var result = await BasicConsumerReceived(ea.Exchange, ea.RoutingKey, ea.Body.ToArray(), queue);
-                if (queue.AutoAck) return;
-                if (result)
-                    _channel.BasicAck(ea.DeliveryTag, queue.AckMultiple);
-                else
-                    _channel.BasicReject(ea.DeliveryTag, queue.RejectRequeue);
-            };
-        }
-        Logger.LogWarning($"RabbitMq:[{RabbitMqOptions.Name}] no queue bind,routeKeys:{string.Join(",", routeKeys)}");
+        _channel.BasicConsume(RabbitMqQueue.Name, RabbitMqQueue.AutoAck, RabbitMqQueue.ConsumerTag, false,
+            RabbitMqQueue.Exclusive,
+            RabbitMqQueue.Arguments, consumer);
+        Logger.LogDebug(
+            $"RabbitMq:[{RabbitMqOptions.Name}] queueName:{RabbitMqQueue.Name} consumer succeeded");
     }
 }
