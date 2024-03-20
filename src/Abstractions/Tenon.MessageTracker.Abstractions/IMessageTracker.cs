a4a9629b2c56b5a80d9ed4408072f6c9b8a38ed9
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-03-20 23:42:01 +0800

    增加DbMessageTracker初步实现 (#32)

--- src/Abstractions/Tenon.MessageTracker.Abstractions/IMessageTracker.cs
+++ src/Abstractions/Tenon.MessageTracker.Abstractions/IMessageTracker.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace Tenon.MessageTracker.Abstractions
 {
     public interface IMessageTracker
     {
         string Name { get; }
-        Task<bool> HasProcessedAsync(long eventId, string trackerName);
-        Task MarkAsProcessedAsync(long eventId, string trackerName);
+        Task<bool> HasProcessedAsync(long eventId, string trackerName, CancellationToken token = default);
+        Task MarkAsProcessedAsync(long eventId, string trackerName, CancellationToken token = default);
     }
 }
