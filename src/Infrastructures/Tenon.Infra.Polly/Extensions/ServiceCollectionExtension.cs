738d093ee76237f78b0d60376216ec3cc0ff22a3
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-03-20 22:17:20 +0800

    增加Polly Infra

---
+++ src/Infrastructures/Tenon.Infra.Polly/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Tenon.Infra.Polly.Helper;
+
+namespace Tenon.Infra.Polly.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IHttpClientBuilder AddHttpPolicyHandlers(this IHttpClientBuilder builder,
+        IEnumerable<IAsyncPolicy<HttpResponseMessage>>? policies = null)
+    {
+        policies ??= new List<IAsyncPolicy<HttpResponseMessage>>
+        {
+            PolicyHelper.AddHttpRetryPolicy(),
+            PolicyHelper.AddHttpTimeoutPolicy(),
+            PolicyHelper.AddHttpCircuitBreakerPolicy()
+        };
+        policies.ToList().ForEach(policy => builder.AddPolicyHandler(policy));
+        return builder;
+    }
+}
---
+++ src/Infrastructures/Tenon.Infra.Polly/Helper/PolicyHelper.cs
@@ -0,0 +1,46 @@
+﻿using Polly;
+using Polly.Timeout;
+
+namespace Tenon.Infra.Polly.Helper;
+
+public sealed class PolicyHelper
+{
+    public static IAsyncPolicy<HttpResponseMessage> AddHttpRetryPolicy()
+    {
+        return Policy.Handle<TimeoutRejectedException>()
+            .OrResult<HttpResponseMessage>(response => (int)response.StatusCode >= 500)
+            .WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromSeconds(3),
+                TimeSpan.FromSeconds(5)
+            });
+    }
+
+    public static IAsyncPolicy<HttpResponseMessage> AddHttpTimeoutPolicy(int timeoutSec = 10)
+    {
+        return Policy.TimeoutAsync<HttpResponseMessage>(timeoutSec);
+    }
+
+    public static IAsyncPolicy<HttpResponseMessage> AddHttpCircuitBreakerPolicy()
+    {
+        var circuitBreakerPolicy = Policy.Handle<Exception>()
+            .CircuitBreakerAsync
+            (
+                10,
+                TimeSpan.FromMinutes(3),
+                (ex, breakDelay) =>
+                {
+                    //todo
+                },
+                () =>
+                {
+                    //todo
+                },
+                () =>
+                {
+                    //todo
+                }
+            );
+        return circuitBreakerPolicy.AsAsyncPolicy<HttpResponseMessage>()
+    }
+}
