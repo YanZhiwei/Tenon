53a3e11857bb0c0cd8d37c1393f40823bb06c496
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-04 23:48:13 +0800

    增加FluentValidationProblemDetails实现

--- src/Extensions/Tenon.FluentValidation.AspNetCore.Extensions/Extensions/ServiceCollectionExtension.cs
+++ src/Extensions/Tenon.FluentValidation.AspNetCore.Extensions/Extensions/ServiceCollectionExtension.cs
@@ -8,10 +8,8 @@ namespace Tenon.FluentValidation.AspNetCore.Extensions.Extensions;
 public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddFluentValidationSetup(this IServiceCollection services,
-        Action<FluentValidationAutoValidationConfiguration>? configurationSetUpAction = null,
         params Assembly[] assemblies)
     {
-        services.AddFluentValidationAutoValidation(configurationSetUpAction);
         services.AddValidatorsFromAssemblies(assemblies);
         return services;
     }
