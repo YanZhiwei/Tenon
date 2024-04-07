800eea0eb8846dac96ad8b3b459a101953cb9467
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-07 23:33:31 +0800

    增加Tenon.Infra.Mongo，Tenon.Caching.Interceptor.Castle初步定义 (#60)

---
+++ src/Tenon.Caching.Interceptor.Castle/Attributes/CachingEvictAttribute.cs
@@ -0,0 +1,7 @@
+﻿using Tenon.Infra.Castle.Attributes;
+
+namespace Tenon.Caching.Interceptor.Castle.Attributes;
+
+public class CachingEvictAttribute : InterceptAttribute
+{
+}
---
+++ src/Tenon.Caching.Interceptor.Castle/CachingAsyncInterceptor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Tenon.Caching.Abstractions;
+using Tenon.Infra.Castle.Attributes;
+using Tenon.Infra.Castle.Interceptors;
+using Tenon.Infra.Castle.Models;
+
+namespace Tenon.Caching.Interceptor.Castle;
+
+public sealed class CachingAsyncInterceptor : InterceptorBase
+{
+    private readonly ICacheProvider _cacheProvider;
+
+    private readonly ILogger<CachingAsyncInterceptor>? _logger;
+
+    public CachingAsyncInterceptor(ICacheProvider cacheProvider, ILogger<CachingAsyncInterceptor>? logger)
+    {
+        _cacheProvider = cacheProvider;
+        _logger = logger;
+    }
+
+    protected override Task InterceptedAsync(InterceptAttribute attribute, InterceptMetadata metadata, Exception? ex)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Intercepted(InterceptAttribute attribute, InterceptMetadata metadata, Exception? ex)
+    {
+        throw new NotImplementedException();
+    }
+}
