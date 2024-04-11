6ff1a5a675eab3cbd48566e6fcff10bcfd9c5642
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-12 00:16:22 +0800

    增加CachingEvictFailedQueue

---
+++ src/Tenon.Caching.Interceptor.Castle/Exceptions/CachingAblException.cs
@@ -0,0 +1,7 @@
+﻿namespace Tenon.Caching.Interceptor.Castle.Exceptions;
+
+public sealed class CachingAblException(string message, string cacheKey, Exception innerException)
+    : Exception(message, innerException)
+{
+    public string CacheKey = cacheKey ?? throw new ArgumentNullException(nameof(cacheKey));
+}
