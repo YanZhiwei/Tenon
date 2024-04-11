030654498ad6ad66594be27bc3bc30c6da758ba8
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-12 00:30:01 +0800

    增加TypeExtension，ReflectionExtension定义

---
+++ src/Extensions/Tenon.Extensions/System/TypeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Tenon.Extensions.System;
+
+public static class TypeExtension
+{
+    public static bool IsTaskWithResult(this TypeInfo typeInfo)
+    {
+        if (typeInfo == null) throw new ArgumentNullException(nameof(typeInfo));
+
+        return typeInfo.GetGenericTypeDefinition() == typeof(Task<>);
+    }
+
+    public static bool IsTask(this TypeInfo typeInfo)
+    {
+        if (typeInfo == null) throw new ArgumentNullException(nameof(typeInfo));
+        return typeInfo.AsType() == typeof(Task);
+    }
+}
