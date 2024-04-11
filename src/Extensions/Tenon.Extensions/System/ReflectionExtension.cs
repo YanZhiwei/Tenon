8a4006dcdbe019b3bf5802f62ccbfc20dfccba96
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-12 00:31:02 +0800

    增加TypeExtension，ReflectionExtension定义 (#66)

---
+++ src/Extensions/Tenon.Extensions/System/ReflectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace Tenon.Extensions.System;
+
+public static class ReflectionExtension
+{
+    public static bool IsReturnTask(this MethodInfo methodInfo)
+    {
+        if (methodInfo == null) throw new ArgumentNullException(nameof(methodInfo));
+        var returnType = methodInfo.ReturnType.GetTypeInfo();
+        return returnType.IsTaskWithResult();
+    }
+}
