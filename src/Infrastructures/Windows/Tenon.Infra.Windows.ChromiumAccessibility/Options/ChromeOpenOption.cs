8a3537846c79d283477a91e3e9ebc4f1a4902815
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-15 23:50:45 +0800

    增加Windows.ChromiumAccessibility定义以及初步实现 (#73)

---
+++ src/Infrastructures/Windows/Tenon.Infra.Windows.ChromiumAccessibility/Options/ChromeOpenOption.cs
@@ -0,0 +1,22 @@
+﻿namespace Tenon.Infra.Windows.ChromiumAccessibility.Options;
+
+public sealed class ChromeOpenOption
+{
+    public Uri Url { get; set; }
+
+    public string AppLocalData { get; set; }
+
+    public HashSet<string>? Args { get; set; }
+
+    /// <summary>
+    /// 是否最大化
+    /// </summary>
+    public bool Maximized { get; set; }
+
+    /// <summary>
+    ///     隐身模式
+    /// </summary>
+    public bool Incognito { get; set; }
+
+    public bool WaitForPageLoad { get; set; }
+}
