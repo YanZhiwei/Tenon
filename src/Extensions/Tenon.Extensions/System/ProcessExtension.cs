99a855c054627ad6d5d5672cc21844a8cae59973
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-15 23:48:39 +0800

    增加Windows.ChromiumAccessibility定义以及初步实现

---
+++ src/Extensions/Tenon.Extensions/System/ProcessExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tenon.Extensions.System;
+
+public static class ProcessExtension
+{
+    public static Task WaitForExitAsync(this Process process,
+        CancellationToken cancellationToken = default)
+    {
+        if (process.HasExited) return Task.CompletedTask;
+
+        var tcs = new TaskCompletionSource<object>();
+        process.EnableRaisingEvents = true;
+        process.Exited += (sender, args) => tcs.TrySetResult(null);
+        if (cancellationToken != default)
+            cancellationToken.Register(() => tcs.SetCanceled(cancellationToken));
+
+        return process.HasExited ? Task.CompletedTask : tcs.Task;
+    }
+}
---
+++ test/Windows/Tenon.Infra.Windows.ChromiumAccessibilityTests/ChromeAccessibilityTests.cs
@@ -0,0 +1,36 @@
+﻿using Tenon.Infra.Windows.ChromiumAccessibility;
+using Tenon.Infra.Windows.ChromiumAccessibility.Options;
+
+[TestClass]
+public class ChromeAccessibilityTests
+{
+    private readonly ChromeAccessibility _chromeAccessibility = new();
+
+    public ChromeAccessibilityTests()
+    {
+        _chromeAccessibility.Detect();
+    }
+
+    [TestMethod]
+    public void DetectTest()
+    {
+        Assert.IsNotNull(_chromeAccessibility.InstallPath);
+    }
+
+    [TestMethod]
+    public void GetMainWindowHandleTest()
+    {
+        Assert.AreNotEqual(_chromeAccessibility.GetMainWindowHandle(), IntPtr.Zero);
+    }
+
+    [TestMethod]
+    public async Task OpenAsyncTest()
+    {
+        await _chromeAccessibility.OpenAsync(new ChromeOpenOption
+        {
+            Url = new Uri("https://www.google.com/"),
+            WaitForPageLoad = true
+        });
+        Assert.AreNotEqual(_chromeAccessibility.GetMainWindowHandle(), IntPtr.Zero);
+    }
+}
