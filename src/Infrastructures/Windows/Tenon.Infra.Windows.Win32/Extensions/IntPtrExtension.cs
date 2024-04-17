7f01787fed29ba7d36cf5b894d72147590f4ac5d
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-17 22:29:37 +0800

    完善Tenon.Infra.Windows.Win32 (#77)

---
+++ src/Infrastructures/Windows/Tenon.Infra.Windows.Win32/Extensions/IntPtrExtension.cs
@@ -0,0 +1,11 @@
+﻿using Windows.Win32.Foundation;
+
+namespace Tenon.Infra.Windows.Win32.Extensions;
+
+internal static class IntPtrExtension
+{
+    public static HWND ToHWnd(this IntPtr intPtrHandle)
+    {
+        return intPtrHandle == IntPtr.Zero ? HWND.Null : new HWND(intPtrHandle);
+    }
+}
---
+++ src/Infrastructures/Windows/Tenon.Infra.Windows.Win32/Models/Rect.cs
@@ -0,0 +1,14 @@
+﻿namespace Tenon.Infra.Windows.Win32.Models;
+
+public struct Rect
+{
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public bool IsEmpty { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+}
