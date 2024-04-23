50c5c7a7ca43bf65d53f3238fd2cc7fb507fb407
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-23 23:26:46 +0800

    增加Tenon.Infra.Swagger定义

---
+++ src/Infrastructures/Tenon.Infra.Swagger/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Tenon.Infra.Swagger.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddBearerAuthenticationHeader(this SwaggerGenOptions swaggerGenOptions)
+    {
+        if (swaggerGenOptions == null)
+            throw new ArgumentNullException(nameof(swaggerGenOptions));
+        swaggerGenOptions.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\""
+        });
+        swaggerGenOptions.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    }
+}
