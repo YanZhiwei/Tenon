7ec2bbc78aec24fd64a575f681a8d0374f03f5d0
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-04-30 21:43:54 +0800

    完善Tenon.Puppeteer.Extensions (#95)

--- src/Extensions/Tenon.Puppeteer.Extensions/IPageExtension.cs
+++ src/Extensions/Tenon.Puppeteer.Extensions/IPageExtension.cs
@@ -15,19 +15,37 @@ public static class PageExtension
         Serializer = new SystemTextJsonSerializer();
     }
 
+    public static string GetPageId(this Page page)
+    {
+        CheckPage(page);
+        return page.MainFrame.Id;
+    }
+
     public static async Task<bool> InjectScriptTagAsync(this IPage page, AddTagOptions options)
     {
-        var htmlContent = await page.GetContentAsync().ConfigureAwait(false);
+        CheckPage(page);
         var jsIdentity = options.Id?.ToString();
+        if (string.IsNullOrEmpty(jsIdentity))
+            throw new ArgumentNullException(nameof(options.Id));
+        if (string.IsNullOrEmpty(options.Content))
+            throw new ArgumentNullException(nameof(options.Content));
+        var htmlContent = await page.GetContentAsync().ConfigureAwait(false);
         if (htmlContent.IndexOf(jsIdentity, StringComparison.OrdinalIgnoreCase) == -1)
             await page.AddScriptTagAsync(options);
         htmlContent = await page.GetContentAsync().ConfigureAwait(false);
         return htmlContent.IndexOf(jsIdentity, StringComparison.OrdinalIgnoreCase) != -1;
     }
 
+    private static void CheckPage(IPage page)
+    {
+        if (page == null)
+            throw new NullReferenceException(nameof(page));
+    }
+
     public static async Task<string> EvaluateFunctionAsync<TReq>(this IPage page,
         PerformRequest<TReq> request)
     {
+        CheckPage(page);
         var evaluateScript = new StringBuilder();
         evaluateScript.Append(request.FunctionName);
         evaluateScript.Append("('");
@@ -36,5 +54,19 @@ public static class PageExtension
         var evaluateResult =
             await page.EvaluateExpressionAsync<string>(evaluateScript.ToString()).ConfigureAwait(false);
         return evaluateResult;
+    }
+
+    public static async Task<bool> IsReadyAsync(this IPage page)
+    {
+        CheckPage(page);
+        var readyState = await page.EvaluateExpressionAsync<string>("document.readyState").ConfigureAwait(false);
+        return readyState.Equals("complete", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static async Task<bool> IsActiveAsync(this IPage page)
+    {
+        CheckPage(page);
+        var readyState = await page.EvaluateExpressionAsync<string>("document.visibilityState").ConfigureAwait(false);
+        return readyState.Equals("visible", StringComparison.OrdinalIgnoreCase);
     }
 }
