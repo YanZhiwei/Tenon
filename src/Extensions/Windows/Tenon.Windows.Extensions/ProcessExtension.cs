0c635e53baaf70b12e8ba5763f9d8d3b29e78c6e
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-06-06 23:50:18 +0800

    新增Tenon.Windows.Extensions并移除冗余项目 (#125)

---
+++ src/Extensions/Windows/Tenon.Windows.Extensions/ProcessExtension.cs
@@ -0,0 +1,17 @@
+﻿using Win32 = Tenon.Infra.Windows.Win32;
+using SysProcess = System.Diagnostics.Process;
+
+namespace Tenon.Windows.Extensions;
+
+public static class ProcessExtension
+{
+    public static IEnumerable<IntPtr> FindCoreWindows(this SysProcess process)
+    {
+        return Win32.Process.FindCoreWindows(process.Id);
+    }
+
+    public static IntPtr FindFirstCoreWindows(this SysProcess process)
+    {
+        return Win32.Process.FindFirstCoreWindows(process.Id);
+    }
+}
--- src/Infrastructures/Windows/Tenon.Infra.Windows.Win32/Process.cs
+++ src/Infrastructures/Windows/Tenon.Infra.Windows.Win32/Process.cs
@@ -1,9 +1,12 @@
-﻿using CsWin32 = Windows.Win32;
+﻿using Tenon.Infra.Windows.Win32.Extensions;
+using CsWin32 = Windows.Win32;
 
 namespace Tenon.Infra.Windows.Win32;
 
 public sealed class Process
 {
+    private const string FrameWindow = "ApplicationFrameWindow";
+
     public static bool TrySetDpiAware(out Exception? ex)
     {
         ex = null;
@@ -16,5 +19,29 @@ public sealed class Process
             ex = e;
             return false;
         }
+    }
+
+    public static IEnumerable<IntPtr> FindCoreWindows(int processId)
+    {
+        var appWindows = new List<IntPtr>();
+        for (var appWindow =
+                 CsWin32.PInvoke.FindWindowEx(IntPtr.Zero.ToHWnd(), IntPtr.Zero.ToHWnd(), FrameWindow, null);
+             appWindow != IntPtr.Zero;
+             appWindow = CsWin32.PInvoke.FindWindowEx(IntPtr.Zero.ToHWnd(), appWindow, FrameWindow, null))
+        {
+            var coreWindow =
+                CsWin32.PInvoke.FindWindowEx(appWindow, IntPtr.Zero.ToHWnd(), "Windows.UI.Core.CoreWindow", null);
+            if (coreWindow == IntPtr.Zero) continue;
+            var corePid = Window.GetProcessId(coreWindow);
+            if (corePid == processId)
+                appWindows.Add(appWindow);
+        }
+
+        return appWindows;
+    }
+
+    public static IntPtr FindFirstCoreWindows(int processId)
+    {
+        return FindCoreWindows(processId).FirstOrDefault();
     }
 }
