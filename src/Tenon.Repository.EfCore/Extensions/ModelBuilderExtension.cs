cbd6861a6796cc3cb6bb5e84cd5fa8e80c0dfca3
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-06-07 23:40:56 +0800

    优化ModelBuilderExtension命名 (#127)

--- src/Tenon.Repository.EfCore/Extensions/ModelBuilderExtension.cs
+++ src/Tenon.Repository.EfCore/Extensions/ModelBuilderExtension.cs
@@ -2,12 +2,12 @@
 
 namespace Tenon.Repository.EfCore.Extensions;
 
-public static class ServiceCollectionExtension
+public static class ModelBuilderExtension
 {
     public static ModelBuilder ApplyConfigurations<TDbContext>(this ModelBuilder modelBuilder)
         where TDbContext : DbContext
     {
-        if (modelBuilder == null) throw new ArgumentNullException(nameof(modelBuilder));
+        ArgumentNullException.ThrowIfNull(modelBuilder);
         foreach (var type in typeof(TDbContext).Assembly.DefinedTypes.Where(t =>
                      t is { IsAbstract: false, IsGenericTypeDefinition: false }
                      && typeof(AbstractEntityTypeConfiguration).IsAssignableFrom(t)))
