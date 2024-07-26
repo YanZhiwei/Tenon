3779845ae8472b8f45f153e0d7e1ae5325e6dadc
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-07-26 21:22:54 +0800

    新增DisplayMonitor实现 (#140)

---
+++ src/Infrastructures/Windows/Tenon.Infra.Windows.Win32/Models/DisplayDevice.cs
@@ -0,0 +1,27 @@
+﻿namespace Tenon.Infra.Windows.Win32.Models;
+
+public sealed class DisplayDevice
+{
+
+    public bool IsPrimary { get; set; }
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public Size RealResolution { get; set; }
+
+    public Size VirtualResolution { get; set; }
+    /// <summary>
+    ///     Color Depth(bits)
+    /// </summary>
+    public uint ColorDepth { get; set; }
+
+    /// <summary>
+    ///     Refresh Rate(Hz)
+    /// </summary>
+    public uint RefreshRate { get; set; }
+
+    public float ScaleY { get; set; }
+
+    public float ScaleX { get; set; }
+}
---
+++ test/Tenon.Infra.Windows.Win32Tests/DisplayMonitorTests.cs
@@ -0,0 +1,23 @@
+﻿using Tenon.Infra.Windows.Win32;
+
+namespace Tenon.Infra.Windows.Win32Tests;
+
+[TestClass]
+public class DisplayMonitorTests
+{
+    [TestMethod]
+    public void FromPointTest()
+    {
+        Point point = new Point(800, 500);
+        var actual = DisplayMonitor.FromPoint(point);
+        Assert.IsNotNull(actual);
+    }
+
+    [TestMethod]
+    public void GetDisplayDevicesTest()
+    {
+        var actual = DisplayMonitor.GetDisplayDevices();
+        Assert.IsNotNull(actual);
+        foreach (var item in actual) Assert.IsNotNull(item);
+    }
+}
